9fb63f0944417c24c4eadf3f69fa9b48071bdbc2
jafrog <howeveririna@gmail.com>
2016-07-24 15:17:59 +0100

    Adds helper methods to the feature spec

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -5,70 +5,56 @@ describe BitmapEditor do
   subject(:bitmap_editor) { described_class.new }
 
   it "Creates an MxN image with all pixels colored white" do
-    command = "I 2 2\n"
+    run_commands_and_show_image
 
-    allow(Kernel).to receive(:gets).and_return(command, BitmapEditor::Command::SHOW, BitmapEditor::Command::EXIT)
-    expect(Kernel).to receive(:puts).with("00\n00")
-    expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Exit::MESSAGE)
+    expect(Kernel).to receive(:puts).with("000\n000\n000")
 
-    bitmap_editor.run(nil, false)
+    run_and_exit
   end
 
   it "Clears the table, setting all pixels to white (O)" do
-    create_image = "I 2 2\n"
     color_pixel = "L 1 1 A\n"
-    command = "C"
+    clear = "C"
 
-    allow(Kernel).to receive(:gets).and_return(create_image, color_pixel, command, BitmapEditor::Command::SHOW, BitmapEditor::Command::EXIT)
-    expect(Kernel).to receive(:puts).with("00\n00")
-    expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Exit::MESSAGE)
+    run_commands_and_show_image(color_pixel, clear)
+    expect(Kernel).to receive(:puts).with("000\n000\n000")
 
-    bitmap_editor.run(nil, false)
+    run_and_exit
   end
 
   it "Colours the pixel (1,1) with colour A" do
-    create_image = "I 2 2\n"
-    command = "L 1 1 A\n"
+    color_pixel = "L 1 1 A\n"
 
-    allow(Kernel).to receive(:gets).and_return(create_image, command, BitmapEditor::Command::SHOW, BitmapEditor::Command::EXIT)
-    expect(Kernel).to receive(:puts).with("A0\n00")
-    expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Exit::MESSAGE)
+    run_commands_and_show_image(color_pixel)
+    expect(Kernel).to receive(:puts).with("A00\n000\n000")
 
-    bitmap_editor.run(nil, false)
+    run_and_exit
   end
 
   it "Draws a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive)" do
-    create_image = "I 3 3\n"
-    command = "V 1 1 2 A\n"
+    draw_vertical = "V 1 1 2 A\n"
 
-    allow(Kernel).to receive(:gets).and_return(create_image, command, BitmapEditor::Command::SHOW, BitmapEditor::Command::EXIT)
+    run_commands_and_show_image(draw_vertical)
     expect(Kernel).to receive(:puts).with("A00\nA00\n000")
-    expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Exit::MESSAGE)
 
-    bitmap_editor.run(nil, false)
+    run_and_exit
   end
 
 
   it "Draws a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive)" do
-    create_image = "I 3 3\n"
-    command = "H 2 3 1 A\n"
+    draw_horizontal = "H 2 3 1 A\n"
 
-    allow(Kernel).to receive(:gets).and_return(create_image, command, BitmapEditor::Command::SHOW, BitmapEditor::Command::EXIT)
+    run_commands_and_show_image(draw_horizontal)
     expect(Kernel).to receive(:puts).with("0AA\n000\n000")
-    expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Exit::MESSAGE)
 
-    bitmap_editor.run(nil, false)
+    run_and_exit
   end
 
   it "Shows the contents of the current image" do
-    create_image = "I 3 3\n"
-    command = "S"
-
-    allow(Kernel).to receive(:gets).and_return(create_image, command, BitmapEditor::Command::EXIT)
+    run_commands_and_show_image
     expect(Kernel).to receive(:puts).with("000\n000\n000")
-    expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Exit::MESSAGE)
 
-    bitmap_editor.run(nil, false)
+    run_and_exit
   end
 
   it "Displays help text" do
@@ -76,18 +62,16 @@ describe BitmapEditor do
 
     allow(Kernel).to receive(:gets).and_return(command, BitmapEditor::Command::EXIT)
     expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Help::MESSAGE)
-    expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Exit::MESSAGE)
 
-    bitmap_editor.run(nil, false)
+    run_and_exit
   end
 
   it "Terminates the session" do
     command = "X\n"
 
     allow(Kernel).to receive(:gets).and_return(command)
-    expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Exit::MESSAGE)
 
-    bitmap_editor.run(nil, false)
+    run_and_exit
   end
 
   it "Doesn't react to unrecognised commands" do
@@ -95,8 +79,19 @@ describe BitmapEditor do
 
     allow(Kernel).to receive(:gets).and_return(command, BitmapEditor::Command::EXIT)
     expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Unknown::MESSAGE)
-    expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Exit::MESSAGE)
 
+    run_and_exit
+  end
+
+  private
+
+  def run_commands_and_show_image(*commands)
+    create = "I 3 3\n"
+    allow(Kernel).to receive(:gets).and_return(create, *commands, BitmapEditor::Command::SHOW, BitmapEditor::Command::EXIT)
+  end
+
+  def run_and_exit
+    expect(Kernel).to receive(:puts).with(BitmapEditor::Command::Exit::MESSAGE)
     bitmap_editor.run(nil, false)
   end
 end
